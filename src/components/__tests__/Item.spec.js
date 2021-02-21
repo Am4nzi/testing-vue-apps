4690390bcc921ec3e7c0e84709592bbbaed54580
Am4nzi <gvs.0027@gmail.com>
2021-02-21 19:32:04 +0000

    initial commit

---
+++ src/components/__tests__/Item.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
+import Item from '../Item.vue'
+describe('Item.vue', () => {
+    test('renders item', () => {
+        const wrapper = shallowMount(Item)
+        debugger
+        expect(wrapper.text()).toContain('item')
+    })
+})
