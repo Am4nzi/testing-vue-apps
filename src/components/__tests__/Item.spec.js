f3f713cec4c34133cbd71985a5d01b965274a7a5
Am4nzi <gvs.0027@gmail.com>
2021-02-21 21:18:22 +0000

    completed up to 3.5

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,9 +1,36 @@
 import { shallowMount } from '@vue/test-utils'
-import Item from '../Item.vue'
-
+import ItemList from '../../views/ItemList.vue'
+import Item from '../../components/Item.vue'
 describe('Item.vue', () => {
-  test('renders item', () => {
-    const wrapper = shallowMount(Item)
-    expect(wrapper.text()).toContain('item')
-  })
+    test('renders item.url', () => {
+        const item = {
+            url: 10
+        }
+        const wrapper = shallowMount(Item, {
+            propsData: { item }
+        })
+        expect(wrapper.text()).toContain(item.url)
+    })
+
+    test('renders a link to the item.url with item.title as text', () => {
+        const item = {
+            url: 'http://some-url.com',
+            title: 'some-title'
+        }
+        const wrapper = shallowMount(Item, {
+            propsData: { item }
+        })
+        const a = wrapper.find('a')
+        expect(a.text()).toBe(item.title)
+        expect(a.attributes().href).toBe(item.url)
+    })
+})
+
+describe('ItemList.vue', () => {
+    test('renders an Item for each item in window.items', () => {
+        window.items = [{}, {}, {}]
+        const wrapper = shallowMount(ItemList)
+        expect(wrapper.findAll(Item))
+            .toHaveLength(window.items.length)
+    })
 })
--- src/views/ItemList.vue
+++ src/views/ItemList.vue
@@ -1,13 +1,18 @@
 <template>
-  <item />
+  <div class="item-list">
+    <item v-for="item in displayItems" :key="item.id"></item>
+  </div>
 </template>
-
 <script>
-import Item from '../components/Item.vue'
-
+import Item from "../components/Item.vue";
 export default {
   components: {
-    Item
-  }
-}
+    Item,
+  },
+  data() {
+    return {
+      displayItems: window.items,
+    };
+  },
+};
 </script>
